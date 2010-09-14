3adb6c03b2deb80aa7120b2285e1a04bafcd424e
Thomas Boetig <tomboetig@gmail.com>
2010-09-14 16:13:24 -0400

    removing check on elem.title for returned objects

--- jquery.embedly.js
+++ jquery.embedly.js
@@ -228,7 +228,7 @@
                 success: function(data) {
                     return $.each(data,
                     function(index, elem) {
-                        return elem.title && !elem.error_code ? embed(elem, batch[index]) : settings.success(null, batch[index]);
+                        return !elem.error_code ? embed(elem, batch[index]) : settings.success(null, batch[index]);
                     });
                 }
             });
